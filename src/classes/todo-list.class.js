f2f21c430a3e19075ac7895e0261775ebb082ae1
bzambrano <brayhanzam@gmail.com>
2021-05-04 00:02:32 -0500

    saving and retrieving tasks

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -1,16 +1,19 @@
 export class TodoList{
 
     constructor(){
-        this.todos=[];
+        //this.todos=[];
+        this.loadLocaStorage();
     }
 
 
     newTodo(todo){
         this.todos.push(todo);
+        this.saveLocaStorage();
     }
 
     deleteTodo(id){
        this.todo = this.todos.filter(todo => todo.id != id);
+       this.saveLocaStorage();
     }
 
     todoComplete(id){
@@ -18,17 +21,30 @@ export class TodoList{
 
     deleteAllTodoCompleted(){
         this.todo = this.todos.filter(todo => !todo.completado);
+        this.saveLocaStorage();
     }
 
     markCompleted(id){
         for(const todo of this.todos){
             if(todo.id  == id){
                 todo.completado = ! todo.completado;
+                this.saveLocaStorage();
                 break;
             }
         }
     }
 
+    saveLocaStorage(){
+        localStorage.setItem('todo',JSON.stringify(this.todos) );
+    }
+
+    loadLocaStorage(){
+
+
+        this.todos = (localStorage.getItem('todo'))?
+        JSON.parse(localStorage.getItem('todo')):
+        [];  
+    }
 
 
 }
--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import {Todo,TodoList} from './classes';
 import {createdTodoHtml} from './js/componentes'; 
 
 export const todoList = new TodoList();
+
+//todoList.todos.forEach(todo => createdTodoHtml( todo ));
+todoList.todos.forEach( createdTodoHtml);// esta es la forma corta de la sentencia anterior
