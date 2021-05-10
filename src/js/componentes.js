b50a9e39aa16e32ddcdf69e583a74e632cefd614
bzambrano <brayhanzam@gmail.com>
2021-05-09 22:23:30 -0500

    functionality is added to the filters

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -3,9 +3,11 @@ import {Todo} from '../classes'
 import {todoList} from '../index'
 
 
-const divTodoList = document.querySelector('.todo-list');
-const txtInput    = document.querySelector('.new-todo');
+const divTodoList             = document.querySelector('.todo-list');
+const txtInput                = document.querySelector('.new-todo');
 const btnDeleteAllCompleted   = document.querySelector('.clear-completed');
+const ulFiltros               = document.querySelector('.filters');
+const anchortFiltros          = document.querySelectorAll('.filtro');
 
 export const createdTodoHtml =(todo) => {
 
@@ -75,4 +77,35 @@ txtInput.addEventListener('keyup', (event) =>{
             }
         }
         
+    });
+
+
+    ulFiltros.addEventListener('click', (event) =>{
+
+        const filtro = event.target.text;
+        if(!filtro) {return} ;
+
+        anchortFiltros.forEach(elem => elem.classList.remove('selected') );
+        event.target.classList.add('selected');
+
+        for (const elemento of divTodoList.children ) {
+
+            elemento.classList.remove('hidden');
+            const completado =  elemento.classList.contains('completed');
+
+            switch (filtro) {
+                case 'Pendientes':
+                    if(completado){
+                        elemento.classList.add('hidden');
+                    }
+                    break;
+
+                case 'Completados':
+                    if(!completado){
+                        elemento.classList.add('hidden');
+                    }
+                    break;
+            
+            }
+        }
     });
